37956b3877a9e98198fdc192dbd81c19fe57d704
miliberlin <michelle.liebheit@posteo.de>
2021-04-01 18:06:50 +0200

    done with scores and varying images

---
+++ src/main.js
@@ -0,0 +1,17 @@
+const game = new Game();
+
+function preload() {
+  game.preload();
+}
+
+function setup() {
+  let canvas = createCanvas(WIDTH, HEIGHT);
+  canvas.parent("canvas");
+}
+
+function draw() {
+  clear();
+  game.drawGrid();
+  game.draw();
+  game.updateScore();
+}
